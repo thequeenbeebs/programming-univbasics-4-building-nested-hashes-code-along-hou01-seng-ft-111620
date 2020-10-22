78113b3f411d8b1bc425559db29d33b628d4a8cc
Blaire Baker <bakerblaire@gmail.com>
2020-10-22 20:13:46 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -7,7 +7,10 @@ def second_challenge
       :hero_friends => []
       },
     :capulet => {
-      #your key/value pairs here!
+      :patriach => {}
+      :matriarch => {}
+      :heroine => {}
+      :heroine_friends => []
       }}
 
 
